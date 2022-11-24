4590204658c80a597f412acd08a8edd76dd00438
quyendv <daovanquyen45@gmail.com>
2022-11-24 23:58:21 +0700

    V9: Associations sequelize
    Add associate in models/user + update findOne in services/user

--- src/models/user.js
+++ src/models/user.js
@@ -9,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            User.belongsTo(models.Role, { foreignKey: 'role_code', targetKey: 'code', as: 'roleData' });
         }
     }
     User.init(
--- src/services/user.js
+++ src/services/user.js
@@ -8,8 +8,15 @@ export const getOne = (userId) =>
             const response = await db.User.findOne({
                 where: { id: userId }, // id là column trong db, hoặc dùng email cũng được vì nó unique
                 attributes: {
-                    exclude: ['password'], // sẽ lấy record (bản ghi) của user ra theo id nhưng k trả về column password làm gì: https://youtu.be/SSCHzHaTnqQ?list=PLGcINiGdJE93CggoN9YBjSnDRV7Rbp3Qu&t=1676
+                    exclude: ['password', 'role_code'], // sẽ lấy record (bản ghi) của user ra theo id nhưng k trả về column password làm gì: https://youtu.be/SSCHzHaTnqQ?list=PLGcINiGdJE93CggoN9YBjSnDRV7Rbp3Qu&t=1676
                 },
+                include: [
+                    {
+                        model: db.Role,
+                        as: 'roleData', // trùng với as trong models/user
+                        attributes: ['id', 'code', 'value'], // attributes có thể là array/object với include/exclude: https://sequelize.org/docs/v6/core-concepts/model-querying-basics/#specifying-attributes-for-select-queries
+                    },
+                ],
             });
 
             resolve({
